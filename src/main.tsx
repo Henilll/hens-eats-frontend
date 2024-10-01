3f83d08903f4365fdfdbea2799753c42d55f0915
Bhavsar Henil A <136498576+Henilll@users.noreply.github.com>
2024-10-01 12:33:40 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import './global.css'
-import { BrowserRouter as Router} from 'react-router-dom';
+import './global.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Toaster } from 'sonner';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
-const queryClient=new QueryClient({
-  defaultOptions :{
-    queries:{
-      refetchOnWindowFocus:false, 
-    }
-  }
-})
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      <QueryClientProvider client={new QueryClient}>
-
-      <Auth0ProviderWithNavigate>
-          <AppRoutes/>
-          <Toaster visibleToasts={1} position='top-right' richColors/> 
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+          <Toaster visibleToasts={1} position="top-right" richColors />
         </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </Router>
-  </StrictMode>,
-)
+  </StrictMode>
+);
